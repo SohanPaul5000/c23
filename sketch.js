7e796979f46503c217ea992be4d051868bac8d28
SohanPaul5000 <72322588+SohanPaul5000@users.noreply.github.com>
2020-11-21 19:18:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,88 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground, wall1, wall2, wall3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+
+	wall1=createSprite(300,600,20,100);
+	wall1.shapeColor="red";
+	wall2=createSprite(500,600,20,100);
+	wall2.shapeColor="red";
+	wall3=createSprite(400,650,200,20);
+    wall3.shapeColor="red";
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+    Matter.Body.setStatic(packageBody,false);
+    
+  }
+
+ if(keyCode === LEFT_ARROW) {
+
+   helicopterSprite.x=helicopterSprite.x-6;
+   translation={x:-6,y:0}
+   Matter.Body.translate(packageBody,translation)
+
+}
+
+if(keyCode === RIGHT_ARROW) {
+
+	helicopterSprite.x=helicopterSprite.x+6;
+	translation={x:+6,y:0}
+	Matter.Body.translate(packageBody,translation)
+ 
+ }
+
+
+}
